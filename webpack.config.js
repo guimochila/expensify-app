b0c1e238c284e119f243ffb5978468e4091fd897
Guilherme <gui.mochila@gmail.com>
2018-06-19 16:33:45 +0100

    Setup production webpack build.

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,52 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: path.join(__dirname, 'src/app.js'),
-  output: {
-    publicPath: "/",
-    path: path.join(__dirname, 'public'),
-    filename: '[name].js',
-  },
-  module: {
-    rules: [
-      {
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s?css$/,
-        use: [
-					'style-loader',
-          'css-loader',
-          'sass-loader'
-				],
-      },
+module.exports = env => {
+  const isProduction = env === 'production';
+  const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' });
+
+  return {
+    entry: path.join(__dirname, 'src/app.js'),
+    output: {
+      publicPath: '/',
+      path: path.join(__dirname, 'public'),
+      filename: '[name].js',
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: { sourceMap: true },
+            },
+            {
+              loader: 'sass-loader',
+              options: { sourceMap: true },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      CSSExtract,
+      new HtmlWebpackPlugin({
+        template: 'public/index.html',
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'public/index.html',
-    }),
-  ],
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true,
-  },
-  mode: 'development',
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+    },
+    mode: isProduction ? 'production' : 'development',
+  };
 };
